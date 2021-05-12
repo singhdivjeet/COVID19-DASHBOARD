f9e05ab9a6082def8e37fcb8b42551246b6c38f8
singhdivjeet <divjeet1@gmail.com>
2021-05-12 17:16:33 +0530

    Build

---
+++ build/precache-manifest.3c101755bcb567869b5a3a800cbdda81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ce52372460e79fc27cd19b682e5a73",
+    "url": "/covid19visuals/index.html"
+  },
+  {
+    "revision": "d8770aa7f66e4658674e",
+    "url": "/covid19visuals/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "900807b3c6901ef8ec0a",
+    "url": "/covid19visuals/static/css/main.6d1900ca.chunk.css"
+  },
+  {
+    "revision": "d8770aa7f66e4658674e",
+    "url": "/covid19visuals/static/js/2.9815454e.chunk.js"
+  },
+  {
+    "revision": "fd8154f77ad230d098e7f14b273e931d",
+    "url": "/covid19visuals/static/js/2.9815454e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900807b3c6901ef8ec0a",
+    "url": "/covid19visuals/static/js/main.9c936096.chunk.js"
+  },
+  {
+    "revision": "5770ffc4b04054e6dc9e",
+    "url": "/covid19visuals/static/js/runtime-main.0a54de83.js"
+  }
+]);
